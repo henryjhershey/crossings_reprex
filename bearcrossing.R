8f644d25783a72b14d2da03005609c43fd2700ae
benjaminhlina <benjamin.hlina@gmail.com>
2023-12-05 13:09:42 -0500

    Create to and from as this is a to and from problem

--- bearcrossing.R
+++ bearcrossing.R
@@ -32,14 +32,83 @@ lines(boundary, lwd = 2,lty = 2, col = "blue")
 # ---- create sf objects ----- 
 # HELP
 # I don't know how to preserve the timestamps of the nodes in the multilinestring.
+# 
+
+
+
+
+
+# ------ This is a to and from problem -----  
+
+
+
+# create duplicate column for destination 
+tracks <- tracks %>% 
+  mutate(
+    to_ts = timestamp,
+    to_lat = location.lat,
+    to_lon = location.long,
+    from_ts = lag(timestamp),
+    from_lat = lag(location.lat),
+    from_lon = lag(location.long),
+  ) %>% 
+  ungroup()
+
+# ts <- lt_switch %>%
+#   group_by(rec_source_ln) %>%
+#   summarise(n = n()) %>%
+#   ungroup()
+# 
+# View(ts)
+
+
+# remove the first time its heard which is NA
+lt <- lt %>% 
+  filter(!(rec_source_group == is.na(rec_source_group)))
+
+# paste to and from toegether 
+lt <- lt %>%
+  mutate(rec_source_to_dest = paste(rec_source_group, rec_dest_group, sep = "-"))
+
+
+
+
+
+
+
+
+
 
 tracks_sf <- tracks %>% 
   st_as_sf(coords = c("location.long", "location.lat"), 
            # agr = "identity", # don't use arg and keep as points for now 
-           crs = 4326)
-  
+           crs = 4326) %>% 
+  group_by(tag.local.identifier, timestamp) %>%
+  summarise(do_union = FALSE) %>%
+  st_cast("LINESTRING") %>% 
+  ungroup()
 
 
+tracks_sf 
+
+# group_by(tag.local.identifier) %>%
+#   summarise(do_union = FALSE) %>%
+#   st_cast("MULTILINESTRING")
+tracks_sf_ls <- tracks %>% 
+  st_as_sf(coords = c("location.long", "location.lat"), 
+           agr = "identity", 
+           crs = 4326) %>% 
+  group_by(tag.local.identifier) %>%
+  summarise(do_union = FALSE) %>%
+  st_cast("MULTILINESTRING")
+
+
+tracks_sf_ls 
+
+
+plot(tracks_sf)
+ggplot() + 
+  geom_sf(data = tracks_sf)
 # group_by(tag.local.identifier) %>%
 #   summarise(do_union = FALSE) %>%
 #   st_cast("MULTILINESTRING")
@@ -49,9 +118,19 @@ glimpse(tracks)
 boundary_sf <- boundary %>% 
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
   summarise(do_union = FALSE) %>%
-  st_cast("LINESTRING")
+  st_cast("polygon")
 
 mapview(tracks_sf) + mapview(boundary_sf)
 
+
+tracks_sf <- tracks_sf %>% 
+  mutate(
+    x_bnd = st_intersects(boundary_sf, tracks_sf, sparse = FALSE)[TRUE]
+  )
+
+
+
+tracks_sf_ls %>% 
+  st_cast("POINT") 
 #find the nodes along individual bear tracks that straddle the boundary line.
 #in the real world example, the line is not perfectly north/south, so can't cheat with hardcoding the longitude
